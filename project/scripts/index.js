49b6b8d231003c325f3fc2a55ddb87a51f0c84b5
Lehi Gomez <lgomez1@byupathway.edu>
2026-07-22 17:12:54 +0800

    ready for styling

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,9 +1,27 @@
 import { loadEpisode, loadEpisodeList } from "./data.js";
 import { renderEpisodeList, renderPlayer } from "./render.js";
-import {
-    initializePlayer,
-    loadEpisodeIntoPlayer
-} from "./player.js";
+import { initializePlayer, loadEpisodeIntoPlayer } from "./player.js";
+import { Icons } from "./icons.js";
+
+document.getElementById("collection-title").textContent =
+    "The Philippines Quezon City North Mission Podcast Archive 2020";
+
+document.getElementById("collection-link").innerHTML = `
+    ${Icons.collection()}
+    About This Collection: The PQCNM Podcast Archive 2020
+    `;
+
+document.getElementById("player-artwork").innerHTML =
+    Icons.artwork(40);
+
+document.getElementById("current-title").textContent =
+    "Select an episode to begin listening";
+
+document.getElementById("player-controls").innerHTML = `
+    <button>${Icons.skipBackward()}</button>
+    <button>${Icons.play(24)}</button>
+    <button>${Icons.skipForward()}</button>
+    `;
 
 const episodes = await loadEpisodeList();
 
