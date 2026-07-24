e58aaeb9def4f9bfd18a39d5899b61a69e507d04
Lehi Gomez <lgomez1@byupathway.edu>
2026-07-24 13:30:55 +0800

    script completed

--- scripts/filetered-temples.js
+++ scripts/filetered-temples.js
@@ -1,3 +1,4 @@
+console.log("JavaScript loaded!");
 const hambutton = document.getElementById('menu');
 const navigation = document.querySelector('.navigation');
 
@@ -5,6 +6,114 @@ hambutton.addEventListener('click', () => {
     hambutton.classList.toggle('open');
     navigation.classList.toggle('open');
 })
+
+// Temples
+const temples = [
+    {
+        templeName: "Aba Nigeria",
+        location: "Aba, Nigeria",
+        dedicated: "2005, August, 7",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "Mexico City Mexico",
+        location: "Mexico City, Mexico",
+        dedicated: "1983, December, 2",
+        area: 116642,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+    },
+    {
+        templeName: "Urdaneta Philippines",
+        location: "Urdaneta, Philippines",
+        dedicated: "2024, April, 28",
+        area: 32604,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/urdaneta-philippines-temple/urdaneta-philippines-temple-45874-main.jpg"
+    },
+    {
+        templeName: "Manila Philippines",
+        location: "Quezon City, Philippines",
+        dedicated: "1981, April, 1",
+        area: 26683,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/_temp/029-Manila-Philippines-Temple.jpg"
+    },
+    {
+        templeName: "Bacolod Philippines",
+        location: "Bacolod, Philippines",
+        dedicated: "2026, May, 31",
+        area: 27895,
+        imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/bacolod-philippines-temple/bacolod-philippines-temple-70237-main.jpg"
+    }
+];
+
+function displayTemples(temples) {
+    const container = document.getElementById("temple-container");
+    container.innerHTML = "";
+
+    temples.forEach((temple) => {
+        const card = document.createElement("section");
+
+        card.innerHTML = `
+        <h2>${temple.templeName}</h2>
+        <p>Location: ${temple.location}</p>
+        <p>Dedicated: ${temple.dedicated}</p>
+        <p>Area: ${temple.area.toLocaleString()} sq ft</p>
+        <img src="${temple.imageUrl}" 
+        alt="${temple.templeName}"
+        loading="lazy">
+        `;
+        container.appendChild(card);
+    });
+}
+
+displayTemples(temples);
+
+
 
 // footer
 const year = document.querySelector("#currentyear");
