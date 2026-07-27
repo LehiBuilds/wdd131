b56f2726a0070959b54c73282aea17b3c570863f
Lehi Gomez <lgomez1@byupathway.edu>
2026-07-27 16:40:35 +0800

    bom functions added

--- week05/bom-app/scripts/bom-scripts.js
+++ week05/bom-app/scripts/bom-scripts.js
@@ -1,29 +1,55 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
 
 button.addEventListener('click', (e) => {
-    e.preventDefault;
+    e.preventDefault();
 
-    const inputValue = input.value;
-    input.value = '';
+    const inputValue = input.value.trim();
 
+    if (inputValue !== '') {
+        displayList(inputValue);
+        chaptersArray.push(inputValue);
+        setChapterList();
+
+        inputValue = '';
+        input.focus();
+    }
+});
+
+function displayList(item) {
     const li = document.createElement('li');
     const deleteButton = document.createElement('button');
 
     li.textContent = inputValue;
     deleteButton.textContent = '❌';
 
+    deleteButton.classList.add('delete');
+    li.append(deleteButton);
+    list.append(li);
 
     deleteButton.addEventListener('click', () => {
-        li.remove();
+        li.removeChild();
+        deleteChapter(li.textContent);
         input.focus;
     })
-    if (inputValue.trim() !== '') {
-        li.appendChild(deleteButton);
-        list.appendChild(li);
-        input.focus;
-    } else {
-        alert('Please make sure that the entry is not empty.');
-    }
-});
+}
+
+function setChapterList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter((item) => item !== chapter);
+    setChapterList();
+}
