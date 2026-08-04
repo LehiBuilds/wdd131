8989f4a32af1f38bd5cf81cc7c134b2971e103c0
Lehi Gomez <lgomez1@byupathway.edu>
2026-08-04 18:42:33 +0800

    form style completed

--- scripts/form.js
+++ scripts/form.js
@@ -4,34 +4,39 @@ const today = new Date();
 const products = [
     {
         id: "fc-1888",
-        name: "flux capacitor",
+        name: "Flux Capacitor",
         averagerating: 4.5
     },
     {
         id: "fc-2050",
-        name: "power laces",
+        name: "Power Laces",
         averagerating: 4.7
     },
     {
         id: "fs-1987",
-        name: "time circuits",
+        name: "Time Circuits",
         averagerating: 3.5
     },
     {
         id: "ac-2000",
-        name: "low voltage reactor",
+        name: "Low Voltage Reactor",
         averagerating: 3.9
     },
     {
         id: "jj-1969",
-        name: "warp equalizer",
+        name: "Warp Equalizer",
         averagerating: 5.0
     }
 ];
 
 const productSelect = document.querySelector("#product-name");
-<select id="product-name">
+products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.name;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
 
-    year.innerHTML = `${today.getFullYear()}`;
+});
+year.innerHTML = `${today.getFullYear()}`;
 
-    document.getElementById("lastModified").innerHTML = document.lastModified;
+document.getElementById("lastModified").innerHTML = document.lastModified;
